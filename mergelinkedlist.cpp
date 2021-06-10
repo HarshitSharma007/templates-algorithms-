6c10b0d41dbe9db0b0772c351f4fe1c895e9c078
Harshit sharma <79695575+HarshitSharma007@users.noreply.github.com>
2021-06-10 17:00:59 +0530

    Update mergelinkedlist.cpp

--- mergelinkedlist.cpp
+++ mergelinkedlist.cpp
@@ -1,31 +1,30 @@
 
-//replace bottom by next :)
-
-Node * merge (Node * a,Node * b){
-    Node * t=new Node(0);
+Node* sortedMerge(Node* a, Node* b)  
+{  
+        Node * t=new   Node(0);
     Node * t2=t;
     while(a&&b){
         if(a->data<b->data){
-            t->bottom=a;
-            a=a->bottom;
-            t=t->bottom;
+            t->next=a;
+            a=a->next;
+            t=t->next;
         }
         else {
-            t->bottom=b;
-            b=b->bottom;
-             t=t->bottom;
+            t->next=b;
+            b=b->next;
+             t=t->next;
         }
     }
     while(a){
-       t->bottom=a;
-            a=a->bottom;
-             t=t->bottom;
+       t->next=a;
+            a=a->next;
+             t=t->next;
     }
     while(b){
-          t->bottom=b;
-            b=b->bottom;
-             t=t->bottom;
+          t->next=b;
+            b=b->next;
+             t=t->next;
     }
     
-    return t2->bottom;
-}
+    return t2->next ;// code here
+}  
